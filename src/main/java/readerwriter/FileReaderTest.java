bb3e6bd86b1d741e21ead1f331bf8be914a46546
kgyhkgyh <kgyhkgyh@163.com>
2015-07-15 09:31:05 +0800

    fileReader

---
+++ src/main/java/readerwriter/FileReaderTest.java
@@ -0,0 +1,29 @@
+package readerwriter;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.nio.CharBuffer;
+
+/**
+ * Created by Administrator on 2015/7/15.
+ */
+public class FileReaderTest {
+
+    public static void main(String[] args) {
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader(new File("d:/test.txt"));
+            CharBuffer buffer = CharBuffer.allocate(100);
+            fileReader.read(buffer);
+            System.out.println(buffer.array());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            IOUtils.closeQuietly(fileReader);
+        }
+    }
+
+}
